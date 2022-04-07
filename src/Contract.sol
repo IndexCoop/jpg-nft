3171525ea64ae66f7b5a318613dc0350f847d2e3
0xbok <1689531+0xbok@users.noreply.github.com>
2022-04-07 23:31:55 +0530

    basic nft setup

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.4;
 
-contract Contract {}
+import {ERC721A} from "erc721a/contracts/ERC721A.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Collectors is ERC721A, Ownable {
+
+    string private _baseTokenURI;
+    constructor() ERC721A("Collectooors", "COLLECTOOORS") {}
+
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+
+    function setBaseURI(string calldata baseURI) external onlyOwner {
+        _baseTokenURI = baseURI;
+    }
+
+    // @notice no `safeMint` allowed implying it doesn't call `onERC721Received` on the minter.
+    function mintBatch(uint256 quantity) external onlyOwner {
+        _mint(msg.sender, quantity, "", false);
+    }
+
+
+}
