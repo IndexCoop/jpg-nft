16303550a3d59744b08e62503306e23e6d8095aa
0xbok <1689531+0xbok@users.noreply.github.com>
2022-04-08 00:51:29 +0530

    add royalty standard

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,19 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.4;
 
-import {ERC721A} from "erc721a/contracts/ERC721A.sol";
+import {ERC721A, URIQueryForNonexistentToken, Strings} from "erc721a/contracts/ERC721A.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC2981} from "@openzeppelin/contracts/interfaces/IERC2981.sol";
 
-contract Collectors is ERC721A, Ownable {
+contract Collectors is ERC721A, Ownable, IERC2981 {
+    using Strings for uint256;
 
+    uint256 public immutable royaltyShare = 5e16; // 5%
     string private _baseTokenURI;
-    constructor() ERC721A("Collectooors", "COLLECTOOORS") {}
 
+    constructor() ERC721A("Collectooors", "COLLECTOOORS") {}
 
     function _baseURI() internal view virtual override returns (string memory) {
         return _baseTokenURI;
     }
-
 
     function setBaseURI(string calldata baseURI) external onlyOwner {
         _baseTokenURI = baseURI;
@@ -24,5 +26,20 @@ contract Collectors is ERC721A, Ownable {
         _mint(msg.sender, quantity, "", false);
     }
 
+    function royaltyInfo(uint256 /* tokenId */, uint256 salePrice) // TODO: check if ERC165 support is needed
+        external
+        view
+        returns (address, uint256) {
+
+        uint256 royaltyAmount = salePrice * royaltyShare / 1e18; // TODO: check for correctness
+        return (owner(), royaltyAmount);
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        if (!_exists(tokenId)) revert URIQueryForNonexistentToken();
+
+        string memory baseURI = _baseURI();
+        return bytes(baseURI).length != 0 ? string(abi.encodePacked(baseURI, tokenId.toString(), ".json")) : '';
+    }
 
 }
